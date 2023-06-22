e532f05f761dd6ef91ed7568bd27b83378aa8f22
kurdsvv <69195904+kurdsvv@users.noreply.github.com>
2023-06-22 11:45:09 +0800

    update

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from app.xvideos import Xvideos
 from app.pornHub import PornHub
 import json
 from app.spankbang import Spankbang
+from fastapi.responses import RedirectResponse
 app = FastAPI()
-
 
 
 
@@ -13,22 +13,22 @@ app = FastAPI()
 async def read_user(file_path: str):
     file_path = "https://www.xvideos.com/" + file_path
     m3u8 = Xvideos(file_path).get_video()
-    return m3u8
+    return RedirectResponse(m3u8)
 
 @app.get("/pornhub/{file_path:path}",response_class=PlainTextResponse)
 async def read_user(file_path: str):
     file_path = "https://cn.pornhub.com/view_video.php?viewkey=" + file_path
     urls = json.loads(PornHub(file_path).get_video())
     if len((urls["quality"][3])) != 0:
-        return (urls["quality"][3]["videoUrl"])
+        return RedirectResponse(urls["quality"][3]["videoUrl"])
     elif ((len((urls["quality"][3])) == 0 )& (len((urls["quality"][2]))!=0)):
-        return (urls["quality"][2]["videoUrl"])
+        return RedirectResponse(urls["quality"][2]["videoUrl"])
     else:
-        return (urls["quality"][1]["videoUrl"])
+        return RedirectResponse(urls["quality"][1]["videoUrl"])
 
 
-@app.get("/Spankbang/{file_path:path}",response_class=PlainTextResponse)
+@app.get("/spankbang/{file_path:path}",)
 async def read_user(file_path: str):
     file_path = "https://spankbang.com/" + file_path
     m3u8=Spankbang(file_path).get_video()
-    return m3u8
+    return RedirectResponse(m3u8)
